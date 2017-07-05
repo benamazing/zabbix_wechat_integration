dc0e59eb5d85d43dfd8ca555624cd61682871586
LIBE5 <ben.x.b.li@cargosmart.com>
2017-07-05 14:58:21 +0800

    add parameter type

--- handle/alert_handler.py
+++ handle/alert_handler.py
@@ -10,8 +10,9 @@ import datetime
 class AlertHandler(tornado.web.RequestHandler):
     def get(self):
         message = self.get_argument('message', '')
+        type = self.get_argument('type', '')
         try:
-            self.send_alert_to_wechat(message)
+            self.send_alert_to_wechat(type, message)
             self.write('%s: Send to Wechat group successfully!' % datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'))
         except Exception, e:
             self.write('%s: Failed to send alert to wechat group! %s' % (datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'), e))
@@ -19,10 +20,10 @@ class AlertHandler(tornado.web.RequestHandler):
     def post(self):
         self.get()
 
-    def send_alert_to_wechat(self, message):
+    def send_alert_to_wechat(self, type, message):
         for group in wechat_group_list:
-            itchat.send('[Zabbix Alert]: %s' % message, toUserName=group[u'UserName'])
+            itchat.send('[%s]: %s' % (type, message), toUserName=group[u'UserName'])
         for user in wechat_user_list:
-            itchat.send('[Zabbix Alert]: %s' % message, toUserName=user[u'UserName'])
+            itchat.send('[%s]: %s' % (type, message), toUserName=user[u'UserName'])
 
 
